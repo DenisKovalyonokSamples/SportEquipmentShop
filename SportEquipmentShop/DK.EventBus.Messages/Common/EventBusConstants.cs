0b9ac8a80757d9822eacae10d617193922c002cf
Denis Kovalyonok <deniskovalyonok@gmail.com>
2025-02-13 13:36:39 +0100

    RabbitMQ event bus implementation.

---
+++ SportEquipmentShop/DK.EventBus.Messages/Common/EventBusConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace DK.EventBus.Messages.Common
+{
+    public class EventBusConstants
+    {
+        //Queue names for RabbitMQ management portal
+        public const string BasketCheckoutQueue = "basketcheckout-queue";
+        public const string BasketCheckoutQueueV2 = "basketcheckout-queue-v2";
+    }
+}
---
+++ SportEquipmentShop/DK.EventBus.Messages/Events/BasketCheckoutEventV2.cs
@@ -0,0 +1,8 @@
+﻿namespace DK.EventBus.Messages.Events
+{
+    public class BasketCheckoutEventV2 : BaseIntegrationEvent
+    {
+        public string? UserName { get; set; }
+        public decimal? TotalPrice { get; set; }
+    }
+}
