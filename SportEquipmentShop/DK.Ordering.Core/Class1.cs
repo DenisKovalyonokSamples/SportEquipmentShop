0c249a7bbc5b45f393d25427a6b4a70be2032e8c
Denis Kovalyonok <deniskovalyonok@gmail.com>
2025-02-13 12:38:36 +0100

    Ordering core types.

--- SportEquipmentShop/DK.Ordering.Core/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DK.Ordering.Core
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ SportEquipmentShop/DK.Ordering.Core/Contracts/IOrderRepository.cs
@@ -0,0 +1,9 @@
+﻿using DK.Ordering.Core.Entities;
+
+namespace DK.Ordering.Core.Contracts
+{
+    public interface IOrderRepository : IAsyncRepository<Order>
+    {
+        Task<IEnumerable<Order>> GetOrdersByUserName(string userName);
+    }
+}
